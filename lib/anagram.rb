36feeaeb494c5cb0ada8e7abd44d75a096d8184a
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-05-15 17:42:14 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+
+  def initialize(word)
+    @word = word
+  end
+
+  def match(anagrams)
+    match = []
+    word.keep_if{ |string| name.split("").sort == string.split("")}
+
+    # some_word.split("")   ==
+  end
+
+  end
