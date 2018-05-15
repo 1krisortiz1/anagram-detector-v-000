820712f4128f110ad01d58b237cd5d61f28ab5e9
Kristin Ortiz <itskristin.ortiz@gmail.com>
2018-05-15 17:42:59 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -7,7 +7,7 @@ class Anagram
 
   def match(anagrams)
   #  match = []
-    word.keep_if{ |string| name.split("").sort == string.split("")}
+    anagrams.keep_if{ |string| name.split("").sort == string.split("")}
 
     # some_word.split("")   ==
   end
